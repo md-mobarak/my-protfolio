d0e903725437cef2bbc71fa87ed00dc7edece37c
mohammad-mobarak-hossen <mdmobarakhossen112@gmail.com>
2022-06-11 23:08:21 +0600

    numberChange

--- src/components/MyProject/Details.js
+++ src/components/MyProject/Details.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Details = ({ detail }) => {
-
-    return (
-        <div>
-            <h1>hello{detail.length}</h1>
-        </div>
-    );
+    const { screenShot-4
+} =detail
+return (
+    <div>
+        <h1>hello{detail.length}</h1>
+    </div>
+);
 };
 
 export default Details;
